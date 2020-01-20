ed6c2c3e715d7b95a9ea8064672c3cb5a524f1fd
taco-8 <>
2020-01-20 17:53:57 +0900

    password_hash

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,7 +12,14 @@ class AdminController extends AppController
 
         if ($this->request->is('post')) {
             $pw = $this->request->data['pw'];
-            if($pw=="test"){
+            
+            /* パスワードハッシュの生成
+            $op = array('cost' => 12);
+            $hashpw = password_hash( $pw, PASSWORD_DEFAULT, $op);
+            echo $hashpw;
+            */
+
+            if(password_verify($pw, '$2y$12$TIJx83iUZeqnr2sFIN5ANO6/79arZj2qTbbT1bkC2MwgxUGnLa9aO')){
                 $this->request->session()->write('adminflg', 'u1');
                 return $this->redirect(['controller'=>'Articles', 'action'=>'index']);
             }
