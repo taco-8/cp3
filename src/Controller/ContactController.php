3bd165ae6f39de2932f5f19bb0550d317f05bbf9
taco-8 <>
2020-01-21 11:24:23 +0900

    send mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Mailer\Email;
 
 class ContactController extends AppController
 {
@@ -9,6 +10,20 @@ class ContactController extends AppController
     public function index()
     {
         $this->viewBuilder()->autoLayout(false);
+
+        //send mail start
+        $email = new Email('default');
+        $frommailadd = "info@xxxxx.com";
+        $tomailadd = "to@mailadd.com";
+        $mailsubject = "this is mail subject";
+        $message = "Hello!\r\n"
+                    ."This is TEST MAIL.";
+		$email->setFrom($frommailadd)
+			->setTo($tomailadd)
+			->setSubject($mailsubject)
+            ->send($message);
+        //send mail end
+
     }
 
 
